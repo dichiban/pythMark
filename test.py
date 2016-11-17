bbd4b911582dcb595c11f1d9130adede7e2aa11b
Daiki Ichiyama <littleyellowman.di@gmail.com>
2016-11-18 04:40:53 +1000

    Now counts how many times each "word" appears in the text file.

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 # Text file to string.
 with open("C:/home/pythMark/texts/sherlock.txt") as f:
     text = f.read()
-count = 0
-for word in text.split():
-    if word == "Sherlock":
-        count += 1
+words = text.split()
+word = set(words)
+word_dic = dict.fromkeys(word, 0)
 
-print(count)
+
+for w in word:
+    word_dic[w] = words.count(w)
+
+print(word_dic)
+#word_dic = dict.fromkeys(set(words), 0)
+#for word in words:
+#    for word2 in word_dic:
+#        if word2 == word:
+#            word_dic[word2] += 1
+
+
+        
+
 
 
