096929bdf05dd48c337d959a00fb3b1f920ece67
Daiki Ichiyama <littleyellowman.di@gmail.com>
2016-11-17 16:29:30 +1000

    Counting the amount of times a specific word appears.

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 # Text file to string.
 with open("C:/home/pythMark/texts/sherlock.txt") as f:
     text = f.read()
+count = 0
+for word in text.split():
+    if word == "Sherlock":
+        count += 1
 
-
-print(len(text.split()))
-print(len(set(text.split())))
+print(count)
 
 
