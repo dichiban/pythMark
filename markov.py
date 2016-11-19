44f65bfdae06de414484b1cc05c71bdfedf676dc
Daiki Ichiyama <littleyellowman.di@gmail.com>
2016-11-19 16:24:53 +1000

    extra text files for testing and rushed random work on markov.py

--- markov.py
+++ markov.py
@@ -2,12 +2,14 @@ from collections import defaultdict
 import re
 import random
 
+DEFAULT_SENTENCE_LENGTH = 10
+
 # Text file to string.
-with open("C:/home/pythMark/texts/sherlock.txt") as f:
+with open("C:/home/pythMark/texts/small_sherlock.txt") as f:
     text = f.read().lower()
 
-text = re.sub('[^a-z\ \']+', " ", text)
-words = list(text.split())
+#text = re.sub('[^a-z\ \']+', " ", text)
+words = text.split()
 word = set(words)
 word_dict = defaultdict(list)
 
@@ -19,8 +21,13 @@ initial = random.randint(0, len(words))
 current = words[initial]
 sentence = [words[initial]]
 
-for i in range(0, 10):
+for i in range(0, 1):
+    buff = word_dict[current][random.randint(0,len(word_dict[current])-1)]
+    while buff[-1] != '.':
+        sentence.append(word_dict[current][random.randint(0,len(word_dict[current])-1)])
+        current = word_dict[current][0]
+        buff = word_dict[current][random.randint(0,len(word_dict[current])-1)]
     sentence.append(word_dict[current][random.randint(0,len(word_dict[current])-1)])
-    current = word_dict[current][0]
 
-print(sentence)
+print(" ".join(sentence))
+#print(word_dict)
